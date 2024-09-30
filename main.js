1cf15ac2a1bc75c658110b990839ad2c2cb50322
shri-ws <eshriee@gmail.com>
2024-10-01 03:03:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-let currentImageIndex = 0;
+const danceStyles = [
+    { style: "Classical", image: "static/img/classical.jpg", description: "Experience the grace of Bharatanatyam and Kathak." },
+    { style: "Bollywood", image: "static/img/bollywood.jpg", description: "Dance to the latest Bollywood hits!" },
+    { style: "Zumba", image: "static/img/zumba.jpg", description: "Get fit with fun and energetic Zumba routines." },
+    { style: "Mehndi", image: "static/img/mehndi.jpg", description: "Celebrate with traditional Mehndi dance performances." },
+    { style: "Wedding Choreography", image: "static/img/wedding.jpg", description: "Perfect moves for your special day." }
+];
 
-function changeBackground(images) {
-    const homeSection = document.querySelector('#home');
-    homeSection.style.backgroundImage = `url(${images[currentImageIndex]})`;
-    currentImageIndex = (currentImageIndex + 1) % images.length;
+let currentStyleIndex = 0;
+const homeSection = document.querySelector('#home');
+const styleTitle = document.querySelector('#style-title');
+const styleDescription = document.querySelector('#style-description');
+
+function changeDanceStyle() {
+    const currentStyle = danceStyles[currentStyleIndex];
+    homeSection.style.backgroundImage = `url(${currentStyle.image})`;
+    styleTitle.textContent = currentStyle.style;
+    styleDescription.textContent = currentStyle.description;
+    currentStyleIndex = (currentStyleIndex + 1) % danceStyles.length;
 }
 
-function startSlideshow(images) {
-    changeBackground(images);
-    setInterval(() => changeBackground(images), 5001);
-}
+// Set initial style
+changeDanceStyle();
 
-fetch('/images')  // Fetch image URLs from the backend
-    .then(response => response.json())
-    .then(images => {
-        startSlideshow(images);
-    });
+// Automatically change style every 5 seconds
+setInterval(changeDanceStyle, 5001);
